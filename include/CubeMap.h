4cc7a4aee39a3228d692332b5820d0b7948a1972
Emily <1207666367@qq.com>
2021-06-08 01:24:03 +0800

    add bounding cube map

--- include/CubeMap.h
+++ include/CubeMap.h
@@ -5,4 +5,20 @@
 #ifndef RAYTRACING_CUBEMAP_H
 #define RAYTRACING_CUBEMAP_H
 
+#include "Texture.h"
+#include "Vector3f.h"
+class CubeMap{
+public:
+///@brief assumes a directory containing
+///left right up down front back.bmp
+    CubeMap(const char * dir);
+    enum FACE
+    {
+        LEFT,RIGHT,UP,DOWN,FRONT,BACK
+    };
+    Vector3f operator()(const Vector3f&);
+    Texture t[6];
+
+};
+
 #endif //RAYTRACING_CUBEMAP_H
